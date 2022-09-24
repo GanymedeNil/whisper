1ad0af55ab24bd2783246448544b6d614084d7cd
GanymedeNil <ganymedenil@gmail.com>
2022-09-24 12:03:46 +0800

    Update __init__.py

--- whisper/__init__.py
+++ whisper/__init__.py
@@ -109,6 +109,6 @@ def load_model(name: str, device: Optional[Union[str, torch.device]] = None, dow
     if torch.cuda.device_count() > 1:
         print("Let's use", torch.cuda.device_count(), "GPUs!")
         model = nn.DataParallel(model)
-    model.load_state_dict(checkpoint["model_state_dict"])
+    model.load_state_dict(checkpoint["model_state_dict"],False)
 
     return model.to(device)
